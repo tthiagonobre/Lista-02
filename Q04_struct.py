d0047cc7db57999d8d85939df96eaf9a4ab905a2
tthiagonobre <128343010+tthiagonobre@users.noreply.github.com>
2023-03-20 01:46:23 -0400

    Delete Q04_struct.py

--- Q04_struct.py
+++
@@ -1,8 +0,0 @@
-def aumento(nome, salario):
-    novo_salario = salario * 1.1
-    return novo_salario
-    
-nome = str(input('Insira o nome: '))
-salario = float(input('Digite o salário: '))
-novo_salario = aumento(nome, salario)
-print(f'Salário de {nome} com 10% de aumento: {novo_salario}')
